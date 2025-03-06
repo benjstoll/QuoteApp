900255f9cbf7de40ac1c0770d51f3bc579cb376e
Benjamin Stoll <bstoll291997@gmail.com>
2025-03-05 19:59:45 -0600

    Implemented my dynamo db methods

--- quote.py
+++ quote.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from os import path
 from yaml import safe_load
 
+
 from gemini_handler import genai_gemini
 from dynamo_handler import dynamo
 
@@ -10,27 +11,36 @@ config_path = path.abspath(path.join(path.dirname(__file__), 'config.yml'))
 with open(config_path, 'r') as f:
     config = safe_load(f)
 
+
 app = Flask(__name__)
 gai = genai_gemini.GenAi(config['gemini_key'], config['prompt'])
+db = dynamo.DynamoDb(config['dynamo_table'])
 
-quote_count = 0
 
 @app.route('/')
 def quote_page():
-    global quote_count
     # Fetch the current quote and count
-    quote = None
+    quote_count = db.get_quote_count()
+    quote = db.get_last_quote(quote_id=quote_count)
+
+    if not quote:
+        quote = "No quotes currently in database, please generate one."
+
     return render_template('quote/quote.html', quote_count=quote_count, quote=quote)
 
 
 @app.route('/generate', methods=['POST'])
 def generate():
     quote = gai.generate_quote()['quote']
+    quote_count = db.get_quote_count()
+    db.insert_quote(quote_id=quote_count+1, quote=quote)
+
     return redirect(url_for('quote_page'))
 
 
 @app.route('/clear', methods=['POST'])
 def clear():
+    db.clear_all_quotes()
     return redirect(url_for('quote_page'))
 
 if __name__ == '__main__':
