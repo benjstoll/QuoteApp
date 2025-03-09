8dbdae2e1eecd073cddd0a4c3779b8b49ae0758b
Benjamin Stoll <bstoll291997@gmail.com>
2025-03-09 11:29:36 -0500

    Converted app to be a package instead of a standalone script

--- QuoteApp/quote.py
+++ QuoteApp/quote.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for
 from os import path
 from yaml import safe_load
 
@@ -11,13 +11,12 @@ config_path = path.abspath(path.join(path.dirname(__file__), 'config.yml'))
 with open(config_path, 'r') as f:
     config = safe_load(f)
 
-
-app = Flask(__name__)
+bp = Blueprint('quote', __name__)
 gai = genai_gemini.GenAi(config['gemini_key'], config['prompt'])
 db = dynamo.DynamoDb(config['dynamo_table'])
 
 
-@app.route('/')
+@bp.route('/')
 def quote_page():
     # Fetch the current quote and count
     quote_count = db.get_quote_count()
@@ -29,19 +28,16 @@ def quote_page():
     return render_template('quote/quote.html', quote_count=quote_count, quote=quote)
 
 
-@app.route('/generate', methods=['POST'])
+@bp.route('/generate', methods=['POST'])
 def generate():
     quote = gai.generate_quote()['quote']
     quote_count = db.get_quote_count()
     db.insert_quote(quote_id=quote_count+1, quote=quote)
 
-    return redirect(url_for('quote_page'))
+    return redirect(url_for('quote.quote_page'))
 
 
-@app.route('/clear', methods=['POST'])
+@bp.route('/clear', methods=['POST'])
 def clear():
     db.clear_all_quotes()
-    return redirect(url_for('quote_page'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect(url_for('quote.quote_page'))
